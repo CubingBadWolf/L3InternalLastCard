1d1727fa04bd53fbb92e017867cc4967d9d10ecc
CubingBadWolf <gaming1dragons@gmail.com>
2023-03-14 14:50:54 -0700

    Added a card for structs and tested it. - Success

--- Card.hpp
+++ Card.hpp
@@ -1,1 +1,41 @@
 #include "includes.h"
+struct Card{
+private:
+public:
+    char suit;
+    char pictureValue;
+    int number;
+    bool Picture;
+    Card(){ // Defines a constructor with no given values ie a blank card
+    }
+    Card(char Suit, int Number){ //Defines a constructor with the values of the card given
+        suit = Suit;
+        number = Number; //assigns the values to the class
+
+        switch(Number){ //This switch allows for assigning whether a card is a picture card/ace or not. 
+            case 1:
+                pictureValue = 'A';
+                Picture = true;
+                break;
+            case 10:
+                pictureValue = 'T';
+                Picture = true;
+                break;
+            case 11:
+                pictureValue = 'J';
+                Picture = true;
+                break;
+            case 12:
+                pictureValue = 'Q';
+                Picture = true;
+                break;
+            case 13:
+                pictureValue = 'K';
+                Picture = true;
+                break;
+            default: //If it is not then the char value will not be a letter hence Picture should be false
+                pictureValue = '0' + number;
+                Picture = false;
+        }
+    }
+};
--- main.cpp
+++ main.cpp
@@ -5,9 +5,22 @@
 
 
 int main(){
-    int numTest = verifyInputs("Enter an integer: ");
-    std::cout << numTest << std::endl;
+    Card card; //testing empty card
+    card.suit = 'S';
+    card.number = verifyInputs("Enter what number for the card: ",1,13); //testing taking in card values from input
 
-    int numTestRange = verifyInputs("Enter an integer between 0 and 10: ", 0,10);
-    std::cout << numTestRange << std::endl;
+    Card card2('D', 2); //testing predefined cards
+    Card Picture('D' ,verifyInputs("Enter a number for a picture card:",1,13)); //Testing whether the picture card switch works
+
+    std::cout << "The first card is " << card.number << card.suit << std::endl; 
+    std::cout << "The second card is " << card2.number << card2.suit << std::endl;
+
+    if(Picture.Picture){ //checking whether assigning picture cards work
+        std::cout << "The picture card is " << Picture.pictureValue << Picture.suit << std::endl;
+    }
+    else{
+        std::cout << "That card is not a picture card. It is " << Picture.pictureValue << Picture.suit << ". Which is the same as " << Picture.number << Picture.suit << std::endl;
+    }
+
+    //outputting the cards to test
 }   
