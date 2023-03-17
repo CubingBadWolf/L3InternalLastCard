743e0a2094b592ac535a5ee5eec5e923405092b6
CubingBadWolf <gaming1dragons@gmail.com>
2023-03-17 14:49:15 +1300

    Deck framework adding

--- Deck.hpp
+++ Deck.hpp
@@ -1,1 +1,24 @@
 #include "includes.h"
+#include "Card.hpp"
+//Deck planning
+
+class Deck{
+private:
+
+public:
+    Deck(){ //constructor, will initilise deck in here
+    }
+
+    void shuffle(){ //deck will need a shuffling function.
+
+    }
+    Card takeCard(int index){ //Take a card from the deck, remove it and return the card value
+
+    }
+    Card takeCard(){ //Overloaded function to take from stack.
+
+    }
+    void gainCard(Card newCard){ //get passed a card to add to the deck
+
+    }
+};
--- main.cpp
+++ main.cpp
@@ -5,22 +5,5 @@
 
 
 int main(){
-    Card card; //testing empty card
-    card.suit = 'S';
-    card.number = verifyInputs("Enter what number for the card: ",1,13); //testing taking in card values from input
-
-    Card card2('D', 2); //testing predefined cards
-    Card Picture('D' ,verifyInputs("Enter a number for a picture card:",1,13)); //Testing whether the picture card switch works
-
-    std::cout << "The first card is " << card.number << card.suit << std::endl; 
-    std::cout << "The second card is " << card2.number << card2.suit << std::endl;
-
-    if(Picture.Picture){ //checking whether assigning picture cards work
-        std::cout << "The picture card is " << Picture.pictureValue << Picture.suit << std::endl;
-    }
-    else{
-        std::cout << "That card is not a picture card. It is " << Picture.pictureValue << Picture.suit << ". Which is the same as " << Picture.number << Picture.suit << std::endl;
-    }
-
-    //outputting the cards to test
+//Will do tests for deck class once started work on functions
 }   
